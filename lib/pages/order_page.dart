2577e5a880445f490390d34d5d3eaca3df3696c6
Yash Chandra Verma <ycverma005@gmail.com>
2021-01-13 00:27:06 +0530

    done with http

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -4,39 +4,30 @@ import 'package:dukaan/widgets/product_page_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class OrderPage extends StatefulWidget {
+class OrderPage extends StatelessWidget {
   static const routeName = '/order';
-
-  @override
-  _OrderPageState createState() => _OrderPageState();
-}
-
-class _OrderPageState extends State<OrderPage> {
-  bool _isLoading = true;
-  @override
-  void initState() {
-    Future.delayed(Duration.zero, () {
-      Provider.of<Orders>(context, listen: false)
-          .fetchAndSetOrders()
-          .then((value) => setState(() {
-                _isLoading = false;
-              }));
-    });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Your Orders"),
-      ),
-      drawer: DrawerProductPage(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Consumer<Orders>(
+        appBar: AppBar(
+          title: const Text("Your Orders"),
+        ),
+        drawer: DrawerProductPage(),
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("Error occured"),
+              );
+            }
+            return Consumer<Orders>(
               builder: (context, orders, _) => ListView.builder(
                 itemBuilder: (ctx, index) {
                   if (orders.noOrdersYet) {
@@ -48,7 +39,8 @@ class _OrderPageState extends State<OrderPage> {
                 },
                 itemCount: orders.orders.length,
               ),
-            ),
-    );
+            );
+          },
+        ));
   }
 }
